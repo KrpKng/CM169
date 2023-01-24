e43d71bcd456838cd6edeeeb92a543deeb96d90f
X.Chris Z <96172043+KrpKng@users.noreply.github.com>
2023-01-23 18:15:25 -0800

    Ver.1.1

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -2,14 +2,22 @@
 
 - sketch.js - 
 == Author: Xingwei Christopher Zhang ==
-== Date: Jan.22, 2023 ==
+== Date: Jan.23, 2023 ==
+
+- README -
+1. press mouse left button to generate random noise image.
+2. press r, g, or b on the keyboard and move your mouse simultaneously to generate lines.
+3. click c on the keyboard to clear the canvas, the background color will randomly change, though all of them should be 'relatively dark'.
+
 
 */
 
+
 var points = [];  // create an array to save elements
 
-var v_Angle = 0.00256;  // velocity of angle change, --bigger v_Angle: smaller curvature.
+var v_Angle = 0.00256;  // velocity of angle change, --the bigger the v_Angle, the smaller the curvature.
 
+// colors in RGB.
 var c_r1;
 var c_r2;
 var c_g1;
@@ -47,6 +55,7 @@ function setup()
     }
 }
 
+
 function draw()
 {
     if (mouseIsPressed)
@@ -55,7 +64,7 @@ function draw()
 
         for (var i = 0; i < points.length; i++)
         {
-            // give it a range
+            // give it a range to generate
             if (dist(width/2, height/2, points[i].x, points[i].y) < 550)
             {
                 // (based on point coordinates in points[]) create square (or any, e.g. ellipse)
@@ -78,29 +87,30 @@ function draw()
         }
     }
     
+    // draw lines
     if(keyIsPressed)
     {
         if (key == 'r')
         {
             // center = center of the canvas
             line(width/2, height/2, mouseX, mouseY);
-            stroke('rgba(255, 0, 0, 0.15)');
+            stroke('rgba(255, 0, 0, 0.1)');
         } 
         if (key == 'g')
         {
             line(width/2, height/2, mouseX, mouseY);
-            stroke('rgba( 0, 255,0, 0.15)');
+            stroke('rgba( 0, 255,0, 0.1)');
         }
         if (key == 'b')
         {
             line(width/2, height/2, mouseX, mouseY);
-            stroke('rgba(0, 0, 255, 0.15)');
+            stroke('rgba(0, 0, 255, 0.1)');
         }
     }
 
     if (keyIsDown(67))
     {
-        clear();
+        clear();  // 'clear' canvas
         setup();  
         // BUT...Unless your computer is powerful enough, 
         // it will get very stuck after you reset it multiple times (or even once).
