b3f5240143c05aabcf5308ac4d54e2bd6d982b42
X.Chris Z <96172043+KrpKng@users.noreply.github.com>
2023-01-30 20:11:35 -0800

    Ver.1.0 --Good!

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -7,61 +7,127 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
 
-// Globals
-let myInstance;
-let canvasContainer;
+let angle1 = 0;
+let angle2 = 0;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+var starsL = [];
 
-    myMethod() {
-        // code to run when method is called
-    }
-}
+var randomN;
+var dirX_Elight;
+var dirY_Elight;
 
-// setup() function is called once when the program starts
+var x;
+var y;
+var z;
+
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(windowWidth,windowHeight, WEBGL);
+  angleMode();
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    background(0, 120, 120);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    rotateY(frameCount * 0.0075);
+    rotateX(frameCount * 0.0075);
+    rotateZ(frameCount * 0.0075);
+    let randomN = random(3,4);
+
+    // Small swirl
+    for (let i = 0; i <= 360; i += 45) {
+        let x = cos(radians(i + angle1)) * 80;
+        let y = sin(radians(i + angle1)) * 65;
+        let z = sin(radians(i + angle1)) * 90;
+        push();
+        translate(x, y, z);
+        sphere(7);
+        pop();
+    }
+
+    // Star, the center
+    sphere(40);
+
+    // Large swirl
+    for (let i = 0; i <= 360; i += 60) {
+        let y = cos(radians(i + angle2)) * 110;
+        let x = sin(radians(i + angle2)) * 80;
+        let z = sin(radians(i + angle2)) * 110;
+        push();
+        translate(x, y, z);
+        sphere(7.5);
+        pop();
+    }
+    
+    for (let i = 0; i <= 360; i += 45) {
+        let z = cos(radians(i + angle2)) * 120;
+        let x = sin(radians(i + angle2)) * 90;
+        let y = sin(radians(i + angle2)) * 150;
+        push();
+        translate(x, y, z);
+        sphere(8);
+        pop();
+    }
+    
+    for (let i = 0; i <= 360; i += 6) {
+        let z = cos(radians(i + angle1)) * 50;
+        let x = sin(radians(i + angle1)) * 50;
+        let y = sin(radians(i + angle1)) * 50;
+        push();
+        translate(x, y, z);
+        box(4);
+        pop();
+    }
+    
+    angle1 -= randomN;
+    angle2 += randomN;
+
+    if (mouseIsPressed === true){
+
+        let dirX_Elight = (mouseX / width) * 3;
+        let dirY_Elight = (mouseY / height) * 3;
+
+        directionalLight(80, 200, 255, -dirX_Elight, -dirY_Elight, -1);
+
+        if (random(100) > 99){
+            for(let i = 0; i < 100; i++){
+                let cR = random(255);
+                let cG = random(255);
+                let cB = random(255);
+
+                let eleColor = color(cR,cG,cB);
+
+                let star = new Star(eleColor);
+                starsL.push(star);
+            }
+        }
+
+        for(let i = starsL.length - 1; i>=0; i--){
+            starsL[i].addVel();
+            starsL[i].showEle();
+        }
+    }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+
+class Star {
+    // construct element
+    constructor(eleColor){
+        this.pos = createVector(0,0,0);
+        this.vel = p5.Vector.random3D().normalize().mult(random(5,6));
+        this.eleColor = eleColor;
+    }
+    // add velocity to the element position
+    addVel(){
+        this.pos.add(this.vel)
+    }
+    // show the created element
+    showEle(){
+        push();
+        noStroke();
+        fill(this.eleColor);
+        translate(this.pos.x, this.pos.y, this.pos.z);
+        sphere(7.5);
+        pop();
+    }
 }
